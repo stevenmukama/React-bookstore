216db85e1da0fce27846a94d2235117bb19f6e37
STEVEN MUKAMA <stevenmukama1@gmail.com>
2023-01-21 12:07:19 +0200

    Add bookstore components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
